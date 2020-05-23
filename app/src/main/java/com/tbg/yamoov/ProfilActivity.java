142cac5ff73eace1eac0786452d5674754eb94a3
Merlin-3D <axel.deffo@gmail.com>
2020-05-23 17:11:02 +0100

    interface profile

--- app/src/main/java/com/tbg/yamoov/ProfilActivity.java
+++ app/src/main/java/com/tbg/yamoov/ProfilActivity.java
@@ -2,13 +2,31 @@ package com.tbg.yamoov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfilActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        btn = findViewById(R.id.joinus);
+
+    }
+
+    public void gotoNext(View view) {
+        Intent dsp = new Intent(ProfilActivity.this,MainActivity.class);
+        startActivity(dsp);
+    }
+
+    public void LognIn(View view) {
+       /* Intent ds = new Intent(ProfilActivity.this,LoginActivity.class);
+        startActivity(ds);*/
+
     }
 }
