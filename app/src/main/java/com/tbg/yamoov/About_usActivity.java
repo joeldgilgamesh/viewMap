4ef59c4a4fee95155908860b4a5ab51fd5e37707
akouchou <michellengono131@gmail.com>
2020-07-06 22:13:04 +0100

    about us

--- app/src/main/java/com/tbg/yamoov/About_usActivity.java
+++ app/src/main/java/com/tbg/yamoov/About_usActivity.java
@@ -1,6 +1,17 @@
 package com.tbg.yamoov;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class About_usActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+
 }
